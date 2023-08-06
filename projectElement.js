b7453dd010ebf56dde218f87d38a123ccd7873b5
Evan Wu <ehw014@ucsd.edu>
2023-08-05 22:41:14 -0700

    last commit?

--- projectElement.js
+++ projectElement.js
@@ -1,29 +1,37 @@
 const localJSON = {
     projects: [
         {   "name" : "Stir Fry with Peppers",
-            "image" : "stir-fry.png",
+            "image" : "img/stir-fry.png",
             "alt" : "Stir Fry Icon",
             "date" : "05/13/2014",
             "description" : "150g Meat (Shrimp, Beef, Pork, Chicken are all good!)\n25g Light Soy Sauce\n20g Oyster Sauce\n10g Fish Sauce\n2 Green Onions\n1/2 Bell Pepper",
             "link" : "stir-fry.html"
         },
         {   "name" : "Omelette",
-            "image" : "omelette.png",
+            "image" : "img/omelette.png",
             "alt" : "Omelette Icon",
             "date" : "06/15/2014",
-            "description" : ".",
+            "description" : "3 Eggs\n1tsp butter\nChoice of filling (totalling 1 cup)\nMeat: Breakfast Sausage, Bacon\nVeggies: Onion, Olives, Peppers, Tomatoes, Spinach, Mushrooms\nExtra Flavor: Parsley, Cheese",
             "link" : "omelette.html"
         },
         {   "name" : "Pancakes",
-            "image" : "pancakes.png",
+            "image" : "img/pancakes.png",
             "alt" : "Pancakes Icon",
             "date" : "06/22/2014",
-            "description" : "we must cook some food, i am so hungry",
+            "description" : "2 cups AP flour\n1/4 cups Sugar\n1 1/3 tbsp Baking Powder\n1/2 tsp Salt\n1 Egg\n1 1/2 cups Milk\n1/4 cups Melted Butter",
             "link" : "pancakes.html"
+        },
+        {   "name" : "Marbled Cheesecake",
+            "image" : "img/cheesecake.png",
+            "alt" : "Cheesecake Icon",
+            "date" : "08/11/2014",
+            "description" : "32 oz Cream Cheese\n3 Eggs\n2/3 cups Sugar\nBaking Chocolate\n2 tsp Vanilla Extract\n1 Tsp Lemon Juice",
+            "link" : "cheesecake.html"
         }
     ]};
 var jsonString = JSON.stringify(localJSON);
 localStorage.setItem('projects', jsonString);
+var loaded = false;
 
 class ProjectCardElement extends HTMLElement {
     constructor() {
@@ -60,13 +68,25 @@ function makeProjectCard(project) {
     shadowImage.setAttribute("alt", `${project.alt}`);
     shadowLink.setAttribute("href", `${project.link}`);
 
-    let oldEle = document.querySelector("h1");
-    oldEle.insertAdjacentElement("afterend", newEle);
+    let oldEle = document.getElementById("cardSection");
+    oldEle.insertAdjacentElement("beforeend", newEle);
 }
 
 
 
 function projectCardLoad(loadMethod) {
+    if(loaded) {
+        if(confirm("You\'ve already loaded some cards into the webpage. Do you wanna do it again?")) {
+            let allCards = document.getElementsByTagName("project-card");
+            while(allCards.length > 0)
+                allCards[0].remove();
+        }
+        else
+            return;
+    }
+    else {
+        loaded = true;
+    }
     let projectData;
     if(loadMethod == "local") {
         const storedJsonString = localStorage.getItem('projects');
@@ -77,8 +97,9 @@ function projectCardLoad(loadMethod) {
         });
     }
     else {
-        const url = 'https://api.jsonbin.io/v3/b/64cf09128e4aa6225ecb6be7'
-        const binId = '64cf09128e4aa6225ecb6be7';
+        let cardSection =document.getElementById("cardSection");
+        cardSection.textContent=("LOADING");
+        const url = 'https://api.jsonbin.io/v3/b/64cf3168b89b1e2299cc2377'
         const secretKey = '$2b$10$BoUwpKBD3EnjPhxmAlyDfeSpDAIGEDmqoGr2dVMQ5mSSegGXF06PW'
         fetch(url, {
             headers: {
@@ -87,9 +108,11 @@ function projectCardLoad(loadMethod) {
             }
         })
         .then(response =>{
+            cardSection.textContent = null;
             return response.json();
         })
         .then(data => {
+            cardSection.textContent = null;
             projectData = data.record.projects;
             projectData.forEach(project => {
                 makeProjectCard(project);
